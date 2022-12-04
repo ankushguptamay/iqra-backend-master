cf179d0a6856279877c4eb9123758f21d61b37ff
ankushguptamay <ankushgupta9675@gmail.com>
2022-12-04 14:58:27 +0530

    add medium's create and read

--- routes/route.js
+++ routes/route.js
@@ -1,10 +1,13 @@
 module.exports = (app) => {
     const level = require('../controllers/Levels/level-cont');
+    const medium = require('../controllers/Mediums/medium-cont');
 
     const router = require('express').Router();
 
     router.post("/add-levels", level.create);
     router.get("/levels", level.findAll);
+    router.post("/add-mediums", medium.create);
+    router.get("/mediums", medium.findAll);
  
     
     app.use("/api/master", router);
